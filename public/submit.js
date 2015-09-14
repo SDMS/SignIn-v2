89875f39d8f75c4a766242bbb70c547fb6c09427
Giselle Koo <gisellegk@gmail.com>
2015-09-14 11:35:12 -0400

    revision of submit functions

--- public/submit.js
+++ public/submit.js
@@ -1,102 +1,88 @@
 var socket = io.connect(null, {'reconnection limit': 3000, 'max reconnection attempts': Number.MAX_VALUE, 'connect timeout':7000});
 
-var selectedComputer = -1;
-var destination = -1;
+var selectedStudent = -1;
 
-var active = true;
-var timeoutID = window.setTimeout(sleep, 600000);
-
-function sleep(){
-        active = false;
-        socket.disconnect();
-        alert("idle");
+function addStudent(data){
+  	var list = document.getElementById('students');
+	var student = document.createElement('li');
+	student.appendChild(document.createTextNode('information goes here'));
+	student.onclick = 'selectStudent(this)';
+	list.appendChild(student);
 }
 
-window.addEventListener("click", checkActive, false);
-
-function checkActive(){
-        if(!active){
-                socket.connect();
-                alert("reconnected!");
-                active = true;
-                window.clearTimeout(timeoutID);
-        }
-
+function removeStudent(data){
+  var list = document.getElementById('students');
+  var student = document.getElementById(data.id); // maybe change.
+  list.removeChild(student);
 }
 
-socket.on('update map', function(student){
-	if(student.action == "sign in") {
-		document.getElementById(student.computer).innerHTML = student.info;
-		document.getElementById(student.computer).className = "computer taken";
-	} else {
-		document.getElementById(student.computer).innerHTML = student.computer;
-		document.getElementById(student.computer).className = "computer"; 
-	}
-		
-});
+function selectStudent(element){
+  deselectStudent();
+  element.className = 'selected';
+  selectedStudent = element.id;
+}
+
+function deselectStudent(){
+	var element = document.getElementById(selectedStudent);
+	element.className = '';
+	selectedStudent = -1;
+}
 
 document.onkeydown = function(e) {
 	if(typeof e == 'undefined' && window.event) {
 		e = window.event;
 	}
 	if(e.keyCode == 13) {
-		if(document.getElementById('map').className == 'signin') { signin(); }
-		else { signout(); }
+      //either sign in or sign out.
 	}
 }
 
 function signin() {
-	if(selectedComputer == -1) {
-		alert('Please select a computer');
-		return;
-	}
 	var sid = document.getElementById('sid').value;
 	if(sid.charAt(0) == 'P') sid = sid.substring(1);
 
-	var student = {"id": sid, "computer": selectedComputer};
+	var student = {"id": sid};
 	console.log(JSON.stringify(student));
 	
 	socket.emit('sign in', student); 	// check if student exists, check if student is already signed in
 }
 
 function signout(){
-    if(selectedComputer == -1 | destination == -1) {
-        alert('Please click on your name and select your destination');
-        return;
-    }
-    var student = {"computer": selectedComputer, "destination": destination};
-    socket.emit('sign out', student);
+  var sid = document.getElementById(selectedStudent);
+  var student = {"id": sid};
+  socket.emit('sign out', student);
 }
 
-socket.on('sign in success', function(student){
-	console.log('signing in.... ' + JSON.stringify(student));
+socket.on('update map', function(student){
+	if(student.action == "sign in") {
+		addStudent(student);
+	}
+	else {
+		removeStudent(student);
+	}
+		
+});
 
-//	alert(student.info + ' at computer ' + selectedComputer);
-	// display student's information
-	document.getElementById(selectedComputer).innerHTML = student.info;
-	document.getElementById(selectedComputer).className = "computer taken";
-	
-	document.getElementById('sid').value = '';
-	selectedComputer = -1;
+socket.on('sign in success', function(data){
+  console.log('signing in.... ' + JSON.stringify(data));
+
+  addStudent(data);
+
+  document.getElementById('sid').value = '';
+
 });
 
 socket.on('sign in fail', function(err){
 	alert('error: ' + err);
 	document.getElementById('sid').value = '';
-	deselectComputer();
-	selectedComputer = -1;
 });
 
-socket.on('sign out success', function(name){
+socket.on('sign out success', function(data){
 	// change computer to empty
-	document.getElementById(selectedComputer).innerHTML = selectedComputer;
-    document.getElementById(selectedComputer).className = "computer";
-    document.getElementById(destination).className = "destination";
-    destination = -1;
-    });
+	removeStudent(data);
+});
     
 socket.on('sign out fail', function(err){
 	alert('error: ' + err);
-	deselectComputer(); // this doesn't do what i think it should do
-	destination = -1;
+	deselectStudent(document.getElementByClassName('selected'));
 });
