68f9f0622606cb8c6a71ef2b72bc36441072f8e2
Giselle Koo <gisellegk@gmail.com>
2015-09-17 15:17:30 -0400

    add room as a param for correct table

--- db.js
+++ db.js
@@ -10,8 +10,9 @@ var db = new sqlite3.Database(file);
 db.serialize(function() {
         if(!exists){
                  console.log("Creating DB file");
-            db.run("CREATE TABLE sdms_021 (id INTEGER, firstName TEXT, lastName TEXT, grade INTEGER, team TEXT, timeIn TEXT, computer INTEGER, fields TEXT)");
-            db.run("CREATE TABLE sdms_lmc (id INTEGER, firstName TEXT, lastName TEXT, grade INTEGER, team TEXT, timeIn TEXT, computer INTEGER, fields TEXT)");
+            db.run("CREATE TABLE sdms-maclab (id INTEGER, firstName TEXT, lastName TEXT, grade INTEGER, team TEXT, timeIn TEXT, computer INTEGER, fields TEXT)");
+            db.run("CREATE TABLE sdms-lmc (id INTEGER, firstName TEXT, lastName TEXT, grade INTEGER, team TEXT, timeIn TEXT, computer INTEGER, fields TEXT)");
+            db.run("CREATE TABLE whs-lmc (id INTEGER, firstName TEXT, lastName TEXT, grade INTEGER, team TEXT, timeIn TEXT, computer INTEGER, fields TEXT");
              } else {
              console.log("DB already exists");
              }
@@ -21,22 +22,22 @@ db.serialize(function() {
 
 //db.close();
 
-module.exports.checkActive = function checkActive(sid, callback){
-	db.get('SELECT * FROM active WHERE id=?', sid, callback);
+module.exports.checkActive = function checkActive(room, sid, callback){
+	db.get('SELECT * FROM ' + room + ' WHERE id=?', sid, callback);
 }
-module.exports.signInStudent = function signInStudent(row, computer){
+module.exports.signInStudent = function signInStudent(room, row, computer){
 	var date = new Date().toLocaleString();
 	console.log(date);
-	db.run('INSERT INTO active VALUES (?,?,?,?,?,?,?,?)', row.id, row.firstName, row.lastName, row.grade, row.team, row.date, row.computer, row.fields);
+	db.run('INSERT INTO ' + room + ' VALUES (?,?,?,?,?,?,?,?)', row.id, row.firstName, row.lastName, row.grade, row.team, row.date, row.computer, row.fields);
 }
-module.exports.checkActiveComputer = function checkActiveComputer(computer, callback){
-	db.get('SELECT * FROM active WHERE computer=?', computer, callback);
+module.exports.checkActiveComputer = function checkActiveComputer(room, computer, callback){
+	db.get('SELECT * FROM ' + room + ' WHERE computer=?', computer, callback);
 }
-module.exports.signOutStudent = function signOutStudent(row, callback) {
-	db.run('DELETE FROM active WHERE id=?', row.id); 
+module.exports.signOutStudent = function signOutStudent(room, row, callback) {
+	db.run('DELETE FROM ' + room + ' WHERE id=?', row.id); 
     callback(); //?
 }
 
-module.exports.getAllActive = function getAllActive(callback){
-	db.all('SELECT * FROM active', callback);
+module.exports.getAllActive = function getAllActive(room, callback){
+	db.all('SELECT * FROM ' + room, callback);
 }
